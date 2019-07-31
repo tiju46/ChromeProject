c4b90e058880e287484bbd1c58d655dad4fc5a56
tiju thomas <tiju thomas@192.168.0.3>
2019-07-31 12:23:45 +0530

    Test Commit

---
+++ MavenProject/src/main/java/NewTest.java
@@ -0,0 +1,54 @@
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.Test;
+
+ 
+public class NewTest {
+ 
+	WebDriver driver;
+ 
+  
+  @Test(priority=1)
+  public void launchbrowser() {
+   
+	  System.out.println("launching Mozilla FireFox browser");
+	  System.setProperty("webdriver.gecko.driver","C:\\Users\\tiju thomas\\Downloads\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+	  FirefoxOptions options = new FirefoxOptions();
+	  options.setCapability("marionette", false);
+	  driver = new FirefoxDriver(options);
+  }
+  
+    
+ //@BeforeMethod
+  public Void bmthd() {
+	
+	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	return null;
+	// TODO Auto-generated method stub
+
+}
+  
+ @Test(priority=2)
+  public void openFFbrowser() {
+		driver.navigate().to("https://apartmentadda.com/user/security.php?is_adda_io=0#/visitor/visitor-in");
+		driver.findElement(By.name("email")).sendKeys("thomas@3five8.com");
+	    driver.findElement(By.name("password")).sendKeys("adda12345");
+	    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	    //click on login button
+	    driver.findElement(By.id("submit_login")).click();
+	}
+ @Test(priority=3)
+ public void closeDriver() {
+		if(driver!=null) {
+			driver.close();
+		}
+	}
+ 
+}
