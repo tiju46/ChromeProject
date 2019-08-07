6a4b21ed9ad34dba95eebf80bda20fbbea84af34
tiju thomas <tiju thomas@192.168.0.5>
2019-08-07 13:25:03 +0530

    test commit

--- MavenProject/src/main/java/NewTest.java
+++ MavenProject/src/main/java/NewTest.java
@@ -1,6 +1,5 @@
 
 import java.net.MalformedURLException;
-import java.lang.interruptedException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -16,7 +15,6 @@ public class NewTest {
   
   @Test(priority=1)
   public void launchbrowser() {
-      Thread.sleep(1000);
 	  System.out.println("launching Mozilla FireFox browser");
 	  System.setProperty("webdriver.gecko.driver","/home/testing/Downloads/geckodriver-v0.24.0-linux64");
 	  FirefoxOptions options = new FirefoxOptions();
@@ -27,7 +25,6 @@ public class NewTest {
   
  @Test(priority=2)
   public void openFFbrowser() {
-	 Thread.sleep(1000);
 		driver.navigate().to("https://apartmentadda.com/user/security.php?is_adda_io=0#/visitor/visitor-in");
 		driver.findElement(By.name("email")).sendKeys("thomas@3five8.com");
 	    driver.findElement(By.name("password")).sendKeys("adda12345");
