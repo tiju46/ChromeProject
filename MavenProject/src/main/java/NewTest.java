f9d89374f2fbc30489c1e610657bb7028ebb13ec
tiju thomas <tiju thomas@192.168.0.5>
2019-08-07 13:45:27 +0530

    test commit

--- MavenProject/src/main/java/NewTest.java
+++ MavenProject/src/main/java/NewTest.java
@@ -18,7 +18,7 @@ public class NewTest {
   public void launchbrowser() {
 	  System.out.println("launching Mozilla FireFox browser");
 	  System.setProperty("webdriver.gecko.driver","/home/testing/Downloads/geckodriver-v0.24.0-linux64/geckodriver");
-	  FirefoxOptions options = new FirefoxOptions()
+	  FirefoxOptions options = new FirefoxOptions();
 	  options.headless=True;
 	  driver = new FirefoxDriver(options);
 	  
