f4df9685ad2fcf4a491f5a95dc9078b9e7da5d74
Tiju Thomas <thomas@3five8.com>
2019-08-08 11:51:28 +0530

    set headless correctly

--- MavenProject/src/main/java/NewTest.java
+++ MavenProject/src/main/java/NewTest.java
@@ -1,5 +1,6 @@
 
 import java.net.MalformedURLException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -8,37 +9,37 @@ import org.testng.annotations.Test;
 //from selenium.webdriver.firefox.options as Options;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
- 
+
 public class NewTest {
- 
-	WebDriver driver;
- 
-  
-  @Test(priority=1)
-  public void launchbrowser() {
-	  System.out.println("launching Mozilla FireFox browser");
-	  System.setProperty("webdriver.gecko.driver","/home/testing/Downloads/geckodriver-v0.24.0-linux64/geckodriver");
-	  FirefoxOptions options = new FirefoxOptions();
-	  options.headless=True;
-	  driver = new FirefoxDriver(options);
-	  
-  }
-   
-  
- //@Test(priority=2)
-  public void openFFbrowser() {
-		driver.navigate().to("https://apartmentadda.com/user/security.php?is_adda_io=0#/visitor/visitor-in");
-		driver.findElement(By.name("email")).sendKeys("thomas@3five8.com");
-	    driver.findElement(By.name("password")).sendKeys("adda12345");
-	   
-	    //click on login button
-	    driver.findElement(By.id("submit_login")).click();
-	}
-// @Test(priority=3)
- public void closeDriver() {
-		if(driver!=null) {
-			driver.close();
-		}
-	}
- 
+
+    WebDriver driver;
+
+
+    @Test(priority = 1)
+    public void launchbrowser() {
+        System.out.println("launching Mozilla FireFox browser");
+        System.setProperty("webdriver.gecko.driver", "/home/testing/Downloads/geckodriver-v0.24.0-linux64/geckodriver");
+        FirefoxOptions options = new FirefoxOptions();
+        options.setHeadless(true);
+        driver = new FirefoxDriver(options);
+    }
+
+
+    //@Test(priority=2)
+    public void openFFbrowser() {
+        driver.navigate().to("https://apartmentadda.com/user/security.php?is_adda_io=0#/visitor/visitor-in");
+        driver.findElement(By.name("email")).sendKeys("thomas@3five8.com");
+        driver.findElement(By.name("password")).sendKeys("adda12345");
+
+        //click on login button
+        driver.findElement(By.id("submit_login")).click();
+    }
+
+    // @Test(priority=3)
+    public void closeDriver() {
+        if (driver != null) {
+            driver.close();
+        }
+    }
+
 }
