8f4634346245c53112c658c1658b77c2dd7a5d7d
Tiju Thomas <thomas@3five8.com>
2019-10-09 15:55:06 +0530

    added try catch block

--- MavenProject/src/main/java/NewTest.java
+++ MavenProject/src/main/java/NewTest.java
@@ -17,15 +17,10 @@ public class NewTest {
 
     @BeforeTest
     public void launch() {
-        //System.out.println("launching Mozilla FireFox browser");
-        //System.setProperty("webdriver.gecko.driver","C:\\Users\\tiju thomas\\Downloads\\geckodriver-v0.24.0-win64\\geckodriver.exe");
-        //System.setProperty("webdriver.gecko.driver", "/home/testing/Downloads/geckodriver-v0.24.0-linux64/geckodriver");
-        //FirefoxOptions options = new FirefoxOptions();
-        //options.setHeadless(false);
-        //driver = new FirefoxDriver(options);
+       
         System.out.println("launching Chrome browser");
-        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\tiju thomas\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", "/home/testing/Downloads/chromedrvr/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver77\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "/home/testing/Downloads/chromedrvr/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
         driver = new ChromeDriver(options);
@@ -44,24 +39,34 @@ public class NewTest {
 
     @Test(priority=2)
     public void CheckIn() throws InterruptedException {
-        System.out.println("*****--- Expected Visitor ---*****");
-        driver.findElement(By.xpath("//ng-multiselect-dropdown[@id='unit_number']")).click();
-        driver.findElement(By.xpath("//div[contains(text(),'200-15')]")).click();
-        driver.findElement(By.xpath("//button[@type='button'][contains(text(),'CHECK IN')]")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("//ng-multiselect-dropdown[@id='reason']")).click();
-        driver.findElement(By.xpath("//div[contains(text(),'Personal')]")).click();
-        driver.findElement(By.xpath("//button[@name='save_visiter']")).click();
-        System.out.println("Test Case Passed ----- Visitor Checked In");
-        Thread.sleep(5000);
+        try {
+            System.out.println("*****--- Expected Visitor ---*****");
+            driver.findElement(By.xpath("//ng-multiselect-dropdown[@id='unit_number']")).click();
+            driver.findElement(By.xpath("//div[contains(text(),'200-15')]")).click();
+            driver.findElement(By.xpath("//button[@type='button'][contains(text(),'CHECK IN')]")).click();
+            Thread.sleep(5000);
+            driver.findElement(By.xpath("//ng-multiselect-dropdown[@id='reason']")).click();
+            driver.findElement(By.xpath("//div[contains(text(),'Personal')]")).click();
+            driver.findElement(By.xpath("//button[@name='save_visiter']")).click();
+            System.out.println("Test Case Passed ----- Visitor Checked In");
+            Thread.sleep(5000);
+        }
+        catch(Exception e) {
+            System.out.println("Test Case failed ----- ");
+        }
     }
 
-    @Test(priority=3)
+   @Test(priority=3)
     public void CheckOut() throws InterruptedException {
+    	try {
         driver.findElement(By.xpath("//a[@href='#/visitor/visitor-out']")).click();
         driver.findElement(By.xpath("//button[@type='button' and contains(., 'CHECK OUT')]")).click();
         System.out.println("Test Case Passed ----- Visitor Checked Out");
         Thread.sleep(5000);
+    	}
+    	catch(Exception e) {
+            System.out.println("Test Case failed ----- ");
+        }
     }
     //@Test(priority=4)
     public void staffcheckin() throws InterruptedException {
